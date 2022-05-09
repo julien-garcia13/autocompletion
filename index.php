af2add724503d843aac511997b27a8252ebd107f
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-05-09 13:35:29 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,27 +1,31 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Section pour les polices -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet">
-    <!-- Fin de section -->
-    <link rel="stylesheet" type="text/css" href="css/autocompletion.css"/>
-    <title>Autocompletion</title>
-</head>
-<body>
-<div class="dark-mode">
-	<input type="checkbox" class="dark-mode-button" id="darkmode" />
-	<label class="button-dark-mode" for="darkmode">
-		<div class="ball"></div>
-	</label>
-</div>
-<div class="titre-zone">
-    <p1 class="titre">Apple Products</p1>
-</div>
-<script src="js/dark-mode.js"></script> <!-- Script pour le dark mode -->
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/autocompletion.css"/>
+        <title>Autocompletion</title>
+    </head>
+    <body>
+        <!-- Boutton pour le dark mode -->
+        <div class="dark-mode">
+            <input type="checkbox" class="dark-mode-button" id="darkmode" />
+            <label class="button-dark-mode" for="darkmode">
+                <div class="ball"></div>
+            </label>
+        </div>
+        <!-- Titre -->
+        <div class="titre-zone">
+            <p1 class="titre">Apple Products</p1>
+        </div>
+        <!-- Barre de recherche -->
+        <div class="barre-de-recherche">
+            <input type="search" name="search_box" placeholder ="Search ..." id="search_box" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onkeyup="javascript:load_data(this.value)"/>
+            <span id="resultat-de-recherche"></span>
+            <div class="search"></div>
+        </div>
+        <script src="js/dark-mode.js"></script> <!-- Script pour le dark mode -->
+        <script src="js/autocompletion.js"></script> <!-- Script pour l'autocomplétion (script principal) -->
+    </body>
 </html>

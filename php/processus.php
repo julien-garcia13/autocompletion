a2b38845b7235ee817e765bc799e65258b49923e
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-05-09 13:36:44 +0200

    Processus

---
+++ php/processus.php
@@ -0,0 +1,18 @@
+<?php
+$db = new PDO("mysql:host=localhost;dbname=autocompletion", "root", ""); // Connexion à la base de données.
+if(isset($_POST["query"]))
+{	
+	$data = array();
+	$condition = preg_replace('/[^A-Za-z0-9\- ]/', '', $_POST["query"]);
+	$query = "SELECT `name`, `year` FROM `products` WHERE `name` LIKE '%".$condition."%' ORDER BY id DESC LIMIT 5";
+	$resultat = $db->query($query);
+	$replace_string = '<b>'.$condition.'</b>';
+	foreach($resultat as $row)
+	{
+		$data[] = array('name' => str_ireplace($condition, $replace_string, $row["name"]));
+		$data[] = array('year' => str_ireplace($condition, $replace_string, $row["year"]));
+	}
+	echo json_encode($data);
+}
+$post_data = json_decode(file_get_contents('php://input'), true);
+?>

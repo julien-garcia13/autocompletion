95ffdb8b82d164da62bdbee22f89ba581d3c3801
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-05-11 12:17:45 +0200

    Add files via upload

--- js/autocompletion.js
+++ js/autocompletion.js
@@ -1,4 +1,4 @@
-function get_text(event)
+function recupererTexte(event)
 {
 	var string = event.textContent;
 	fetch("php/processus.php",
@@ -15,38 +15,33 @@ function get_text(event)
         }
 	}
 	)
-	.then(function(response)
+	.then(function(reponse)
     {
-		return response.json();
-	}
-	)
-	.then(function(responseData)
-	{
 		document.getElementsByName('search_box')[0].value = string;
 		document.getElementById('resultat-de-recherche').innerHTML = '';
 	}
-	);
+	)
 }
-function load_data(query)
+function chargementDonnees(query)
 {
 	if(query.length > 2)
 	{
-		var form_data = new FormData();
-		form_data.append('query', query);
+		var via_donnees = new FormData();
+		via_donnees.append('query', query);
 		var requete_ajax = new XMLHttpRequest();
 		requete_ajax.open('POST', 'php/processus.php');
-		requete_ajax.send(form_data);
+		requete_ajax.send(via_donnees);
 		requete_ajax.onreadystatechange = function()
 		{
 			if(requete_ajax.readyState == 4 && requete_ajax.status == 200)
 			{
-				var response = JSON.parse(requete_ajax.responseText);
+				var reponse = JSON.parse(requete_ajax.responseText);
 				var html = '<div class="liste-de-recherche">';
-				if(response.length > 0)
+				if(reponse.length > 0)
 				{
-					for(var count = 0; count < response.length; count++)
+					for(var count = 0; count < reponse.length; count++)
 					{
-						html += '<a href="#" class="resultat" onclick="get_text(this)">'+response[count].name+'</a><br />';
+						html += '<a href="#" id="myBtn" class="resultat" onclick="recupererTexte(this)">'+reponse[count].name+'</a><br />';
 					}
 				}
 				else // Si aucun produit n'est trouvé dans la recherche

ac4dc171a91c19c249236a13890d156bb1d28eb3
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-05-28 18:57:52 +0200

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,4 +1,10 @@
 <?php // Fichier à lier avec un "require_once"
+// Section pour les erreurs (logs)
+error_reporting(-1); // Montre tous les erreurs
+ini_set("display_errors", "1");
+ini_set("log_errors", 1);
+ini_set("error_log", "php-error.log"); // Fichier du log
+// Fin de section
 class BaseDeDonnées // Classe à appeler pour faire fonctionner une page qui demande la base de données au fonctionnement.
 {
     private $host = 'localhost';
@@ -18,6 +24,7 @@ class BaseDeDonnées // Classe à appeler pour faire fonctionner une page qui de
         try
         {
             $this->db = new PDO('mysql:host='.$this->host.';dbname='.$this->database, $this->username, $this->password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8', PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+            $this->db-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         catch(PDOException $messageErreur)
         {
